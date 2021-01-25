2d0cc197c50bdbcf677954f20717789544357d55
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2021-01-25 14:40:39 -0700

    File Starts
    Starting code

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ // Create a Phrase class to handle the create of phrases
+
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase;
+  }
+}
+
+
+/*
+
+phrase: This is the actual phrase the Phrase object is representing. This property
+should be set to the `phrase` parameter, but converted to all lower case.
+
+*/
--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+/*          Pseudocode / Instructions         */
+
+// Create a new instance of the `Game` class and add event listeners for the
+// start button and onscreen keyboard buttons
+
+const start = document.querySelector(".start");
+const keyboardButtons = document.querySelector("#qwerty");
+//const keyboardButtons2 = document.querySelector(".keyrow");
+
+start.addEventListener("click", event => {});
+keyboardButtons.addEventListener("keyup", event => {});
+//keyboardButtons2.addEventListener("keyup", event => {});
+
+class Game {
+
+}
+
+
+
+
+
+
+/*          Testing Code         */
+// const phrase = new Phrase();
+// const game = new Game();
+
+// const phrase = new Phrase('Life is like a box of chocolates');
+// console.log(`Phrase - phrase: ${phrase.phrase}`);
